aa1dbc6a97ecc9c8ca6aed1352bc730c28a430bf
Luther Karper <luther.karper@live.com>
2020-08-13 15:02:00 -0400

    Add authRouter and AuthService

---
+++ src/auth/auth-service.js
@@ -0,0 +1,34 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+
+const AuthService = {
+    getUserWithUserName(db, user_name) {
+        return db('admins')
+            .where({ user_name })
+            .first();
+    },
+    comparePasswords(password, hash) {
+        return bcrypt.compare(password, hash);
+    },
+    parseBasicToken(token) {
+        return Buffer
+            .from(token, 'base64')
+            .toString()
+            .split(':');
+    },
+    createJWT(subject, payload) {
+        return jwt.sign(payload, config.JWT_SECRET, {
+            subject,
+            expiresIn: config.JWT_EXPIRY,
+            algorithm: 'HS256',
+        });
+    },
+    verifyJWT(token) {
+        return jwt.verify(token, config.JWT_SECRET, {
+            algorithms: ['HS256'],
+        });
+    },
+}
+
+module.exports = AuthService;
