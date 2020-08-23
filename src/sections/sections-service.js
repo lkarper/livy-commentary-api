0a6fefe1ed133724a699f86d465f5b74d5ccffb7
Luther Karper <luther.karper@live.com>
2020-08-23 14:57:06 -0400

    Update SectionsService

--- src/sections/sections-service.js
+++ src/sections/sections-service.js
@@ -23,11 +23,30 @@ const SectionsService = {
     },
     getAllSectionNumbers(db) {
         return db
-            .from('commentary_chapter_sections AS ccs')
-            .select(
-                'ccs.id',
-                'ccs.section_number'
-            );
+        .from('commentary_books as cb')
+        .select(
+            'cb.id',
+            'cb.book_number',
+            db.raw(
+                `(
+                    select array_to_json(
+                        array_agg(row_to_json(h))
+                    ) from (
+                        select id, book_number, chapter_number, chapter_title, (
+                            select array_to_json(
+                                array_agg(row_to_json(h))
+                            ) from (
+                                select id, section_number
+                                from commentary_chapter_sections AS ccs
+                                where chap.chapter_number=ccs.chapter_number
+                            ) h
+                        ) as sections
+                        from commentary_chapters AS chap
+                        where cb.book_number=chap.book_number    
+                    ) h
+                ) as chapters`
+            )
+        )
     },
     getSectionByNumber(db, section_number) {
         return db
